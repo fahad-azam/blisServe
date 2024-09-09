741649cf3acf07d928c6915dce2d9a4073d83aa0
Fahad Azam <azamfahad611@gmail.com>
2024-09-09 16:18:38 +0530

    setup tailwind

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Navbar</h1>
+    </div>
+  )
+}
+/*https://codepen.io/Haikaladitya6/pen/wvOKpdm*/
+// https://codepen.io/Koziuk-S/pen/zEwXYg
+export default Navbar
